4cc4c66f09acf21c47fd05537ece15680cbd3c4e
SymbolixAU <dcooley@symbolix.com.au>
2020-01-17 09:01:02 +1100

    tests

--- tests/testthat/test-sf_point.R
+++ tests/testthat/test-sf_point.R
@@ -29,6 +29,15 @@ test_that("sf_points works for various objects",{
   res <- sfheaders:::rcpp_sf_point( x, c(0,1), TRUE )
   expect_true( all( attr(res, "class") == c("sf", "data.frame") ) )
 
+  x <- as.matrix( as.data.frame( matrix( c(1:10) , ncol = 2 ) ) )
+  res <- sfheaders:::rcpp_sf_point( x, c("V1","V2"), TRUE )
+  expect_true( all( attr(res, "class") == c("sf", "data.frame") ) )
+
+  x <- as.matrix( as.data.frame( matrix( c(1:10) , ncol = 2 ) ) )
+  x[1,1] <- 1.1
+  res <- sfheaders:::rcpp_sf_point( x, c("V1","V2"), TRUE )
+  expect_true( all( attr(res, "class") == c("sf", "data.frame") ) )
+
   x <- matrix( c(1:12) , ncol = 3 )
   res <- sfheaders:::rcpp_sf_point( x, c(0,1), TRUE )
   expect_true( all( attr(res, "class") == c("sf", "data.frame") ) )
