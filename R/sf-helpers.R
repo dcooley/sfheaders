e90ea93be35ba621a57bcfd2762218647e4cbd98
Michael Sumner <mdsumner@gmail.com>
2020-03-15 20:10:57 +1100

    specification

--- R/sf-helpers.R
+++ R/sf-helpers.R
@@ -9,30 +9,76 @@ name_matcher <- function(x, ...) {
                polygon_id = "polygon_id",
                multilinestring_id = "multilinestring_id",
                linestring_id = "linestring_id",
-               multipoint_id = "multipoint_id",
-               point_id = "point_id")
+               multipoint_id = "multipoint_id")
   ## return the list args that make sense for this data frame
   args[base::intersect(colnames(x), names(args))]
 }
 
 
-#' Helper functions to make sf
+#' Helper for sf POINT
 #'
-#' These are simpler versions of the sfheaders creators:
+#' Constructs sf of POINT objects, a helper for [sf_point()] with a simpler syntax.
 #'
-#' less strict about arguments
-#' assuming default argument names
-#' using z, m if present
-#' using `close = FALSE` as default
-#' using `keep = FALSE` same as proper creators
-#' we don't allow partial name matching
+#' These are simpler versions of the main functions [sf_point()], [sf_multipoint()],
+#' [sf_linestring()], [sf_multilinestring()], [sf_polygon()], and [sf_multipolygon()] for
+#' input data frame or matrix that contains columns appropriately of 'x', 'y', 'z', 'm',
+#' ' multipolygon_id', polygon_id', 'multilinestring_id', 'linestring_id', 'multipoint_id'.
+#' Some columns are mandatory, such as 'x' and 'y' (always), while others depend on the output
+#' type where each column for that type is mandatory. The 'z' and/or 'm' values are included
+#' for 'XYZ', 'XYM', or 'XYZM' geometry types if and as they are present.
+#'
+#' In summary these helpers:
+#'
+#' * not strict about arguments for column names.
+#' * use assumed default column names, with no variation or absence allowed for a given type.
+#' * use `z`, and/or `m` if present.
+#' * use `close = FALSE` as default
+#' * use `keep = FALSE` same as proper creators
+#' * unlike [sf_point()] [sf_pt()] does not accept a flat vector
+#' * require a matrix or data frame with complete column names
+#'
+#' None of the helpers allow partial name matching for column names.
+#'
+#' @inheritParams sf_point
+#' @inheritSection sfc_point notes
+#' @inheritSection sf_point Keeping Properties
+#' @return \code{sf} object of POINT geometries
+#' @examples
+#'
+#' x <- cbind(x = 1, y= 3)
+#' sf_pt( x )
+#' sf_pt(cbind(x, z = 2))
+#'
+#' x <- matrix( c(1:10) , ncol = 2 )
+#' sf_pt( x )
+#'
+#' x <- setNames( as.data.frame( x ), c("x","y") )
+#' sf_point( x )
+#' sf_point( obj = x, x = "x", y = "y" )
+#' sf_point( obj = x, x = "y", y = "x" )
+#'
+#' # keeping properties
+#' x$val <- letters[1:5]
+#' sf_point( x, x = "x", y = "y", keep = TRUE )
+#'
+#' @export
+sf_pt <- function(obj, keep = FALSE, ...) {
+  stopifnot(all(c("x", "y") %in% colnames(obj)))
+  call_args <- name_matcher(obj)
+  call_args[c("obj", "keep")] <- list(obj, keep)
+  ## needs rethinking
+  call_args[c("multipolygon_id", "polygon_id", "multilinestring_id", "linestring_id", "multipoint_id")] <- NULL
+  do.call(sfheaders::sf_point, call_args)
+}
+
+
 sf_mpoly <- function(obj, close = FALSE, keep = FALSE, ...) {
   ## determine minimum names required
   stopifnot(all(c("x", "y", "multipolygon_id", "polygon_id", "linestring_id") %in% colnames(obj)))
   call_args <- name_matcher(obj)
   call_args[c("obj", "keep", "close")] <- list(obj, keep, close)
   ## needs rethinking
-  call_args[c("multipoint_id")] <- NULL
+  call_args[c("multilinestring_id", "multipoint_id")] <- NULL
 
   do.call(sfheaders::sf_multipolygon, call_args)
 }
@@ -69,11 +115,4 @@ sf_mpt <- function(obj, keep = FALSE, ...) {
   call_args[c("multipolygon_id", "polygon_id", "multilinestring_id", "linestring_id")] <- NULL
   do.call(sfheaders::sf_multipoint, call_args)
 }
-sf_pt <- function(obj, keep = FALSE, ...) {
-  stopifnot(all(c("x", "y") %in% colnames(obj)))
-  call_args <- name_matcher(obj)
-  call_args[c("obj", "keep")] <- list(obj, keep)
-  ## needs rethinking
-  call_args[c("multipolygon_id", "polygon_id", "multilinestring_id", "linestring_id", "multipoint_id")] <- NULL
-  do.call(sfheaders::sf_point, call_args)
-}
+
--- tests/testthat/test-sf_creator-helpers.R
+++ tests/testthat/test-sf_creator-helpers.R
@@ -3,7 +3,9 @@ context("sf_creator-helpers")
 test_that("sf_mpoly works",{
   m <- matrix(c(0, 0, 1, 1, 0,
               0, 1, 1, 0, 0), ncol = 2, dimnames = list(NULL, c("x", "y")))
-  mat <- cbind(m, multipolygon_id = 1, polygon_id = 1, linestring_id = 1)
+  mat <- cbind(m, multipolygon_id = 1, polygon_id = 1,
+                  multilinestring_id = 1, linestring_id = 1,
+                  multipoint_id = c(1, 1, 2, 2, 2), point_id = 1:5)
   res <- sf_mpoly(mat)
   ans <- sf_multipolygon(mat, x = "x", y = "y", multipolygon_id = "multipolygon_id", polygon_id = "polygon_id", linestring_id = "linestring_id")
   expect_equal(res,
@@ -18,11 +20,11 @@ test_that("sf_mpoly works",{
   ## zm works
   expect_true(dim(sf_mpoly(cbind(mat, z = 2, m = 0))[["geometry"]][[1L]][[1L]][[1L]])[2L] == 4L)
   ## keep works
-  expect_equal(dim(sf_mpoly(cbind(mat, m = 0, cactus = 1:5))), c(1L, 2L))
+  expect_equal(dim(sf_mpoly(cbind(mat, m = 0, cactus = 1:5, keepmetoo = 2))), c(1L, 2L))
   kept <- sf_mpoly(cbind(mat, m = 0, cactus = 1:5), keep = TRUE)
-  expect_equal(dim(kept), c(1L, 3L))
+  expect_equal(dim(kept), c(1L, 6L))
 
   kept2 <- sf_mpoly(cbind(mat, m = 0, cactus = 1:5, keepmetoo = 2), keep = TRUE)
-  expect_equal(dim(kept2), c(1L, 4L))
+  expect_equal(dim(kept2), c(1L, 7L))
 
 })
