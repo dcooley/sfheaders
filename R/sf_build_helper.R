76c19b7ad8778da1957ac1d612c623e8850dabff
Michael Sumner <mdsumner@gmail.com>
2020-04-01 20:51:42 +1100

    build helper

--- R/sf_build_helper.R
+++ R/sf_build_helper.R
@@ -19,19 +19,31 @@ gtype_matcher <- function(x) {
                 "multipoint_id")
 
 
+  # if (!any(elements %in% x)) {
+  #   return("POINT")
+  # }
+  if (all(elements[c(1, 2, 4)] %in% x)) {
+    return("MULTIPOLYGON")
+  }
+  if (all(elements[c(2, 4)] %in% x)) {
+    return("POLYGON")
+  }
+  if (all(elements[c(3, 4)] %in% x)) {
+    return("MULTILINESTRING")
+  }
+  if ("linestring_id" %in% x) {
+    return("LINESTRING")
+  }
 
-
-  ## FIXME: names(df) == x,y,multipoint_id,multipolygon_id won't work
-  # enc <- paste_index(match(unique(x), elements))
-  # switch(enc,
-  #        "124" = "MULTIPOLYGON",
-  #        "24" = "POLYGON",
-  #        "34" = "MULTILINESTRING",
-  #        "4" = "LINESTRING",
-  #        "5" = "MULTIPOINT",
-  #        "6" = "POINT")
-  #
-
+  ## not sure, preference this or not? (probably not, put this last)
+  if ("multipoint_id" %in% x) {
+    return("MULTIPOINT")
+  }
+  ## only need x,y for POINT
+  if (all(c("x", "y") %in% x)) {
+    return("POINT")
+  }
+  stop(sprintf("cannot determine geometry type from %s", sort(x)))
 }
 gdim_matcher <- function(x) {
   elements <- c("x", "y", "z", "m")
@@ -75,23 +87,40 @@ construct_matcher <- function(x) {
 #' @export
 #'
 #' @examples
-# df <- data.frame(
-#   ml_id = c(1,1,1,1,1,1,1,1,2,2,2,2,2)
-#   , l_id = c(1,1,1,2,2,3,3,3,1,1,1,2,2)
-#   , lon = rnorm(13)
-#   , lat = rnorm(13)
-#   , zz = rnorm(13)
-#   , mm = rnorm(13)
-# )
-#
-# #library(dplyr)
-# #df %>% transmute(x = lon, y = lat, m = mm,
-# #                 multilinestring_id = ml_id,
-# #                 linestring_id = l_id) %>% to_sf()
-#
-# #df %>% transmute(linestring_id = l_id, y = lat, x = lon, z = zz,
-# #                 multipolygon_id = ml_id,
-# #                 polygon_id = ml_id) %>% to_sf()
+#' df <- data.frame(
+#'   ml_id = c(1,1,1,1,1,1,1,1,2,2,2,2,2)
+#'   , l_id = c(1,1,1,2,2,3,3,3,1,1,1,2,2)
+#'   , lon = rnorm(13)
+#'   , lat = rnorm(13)
+#'   , zz = rnorm(13)
+#'   , mm = rnorm(13)
+#' )
+#'
+#' library(dplyr); library(sf)
+#' ## specify multlinestring
+#' df %>% transmute(x = lon, y = lat, m = mm,
+#'                 multilinestring_id = ml_id,
+#'                 linestring_id = l_id) %>% to_sf()
+#'
+#' ## round trip
+#' sf_to_df(silicate::minimal_mesh) %>% to_sf()
+#'
+#' ## linestring (have to hack, it's not realistic)
+#' sf_to_df(silicate::minimal_mesh) %>%
+#' transmute(m = 1, y, x, linestring_id) %>%
+#'  arrange(linestring_id) %>% ## needed because we are hacking mpoly, not realistic necessarily
+#' to_sf()
+#'
+#' ## we get POINT
+#' sf_to_df(silicate::minimal_mesh) %>% transmute(y, x) %>%
+#' to_sf()
+#'
+#' ## MULTIPOINT
+#' sf_to_df(silicate::minimal_mesh) %>%
+#'  transmute(x, y, multipoint_id = row_number() > 3, polygon_id) %>% ## need linestring also to get polygon
+#'  to_sf()
+#'
+#'
 to_sf <- function(x, debug = TRUE) {
   gtype <- gtype_matcher(names(x))
   if (debug) {
